eeee3ba3a54eb701ed96a1c56e49baeb106036c9
Sathya priyaa AS <98306166+Priyaashankar@users.noreply.github.com>
2023-06-10 17:25:18 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,68 +1,14 @@
-import java.sql.Struct;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
+        Son son = new Son(30);
+        son.career();
 
-        int[] numbers = new int[5];
-        String[] names = new String[5];
-        int[] rno = new int[5];
-        String[] name = new String[5];
-        float[] marks = new float[5];
+        son.normal();
 
-        Student[] students = new Student[5];
+        Parent daughter = new Daughter(28);
+        daughter.career();
 
-        Student Sona = new Student(15, "Sona sri", 85.4f);
-        Student priya = new Student(18, "Sathya priyaa", 90.3f);
-
-        System.out.println(Sona.rno);
-        System.out.println(Sona.name);
-        System.out.println(Sona.marks);
-
-        Student random = new Student(priya);
-        System.out.println(random.name);
-
-        Student random2 = new Student();
-        System.out.println(random2.name);
-
-        Student one = new Student();
-        Student two = one;
-
-        one.name = "Something something";
-
-        System.out.println(two.name);
-
+        Parent.hello();
+        // Parent mom = new Parent(45);
     }
-}
-
-class Student {
-    int rno;
-    String name;
-    float marks = 90;
-
-    void greeting() {
-        System.out.println("Hello! My name is " + this.name);
-    }
-
-    void changeName(String name) {
-        this.name = name;
-    }
-
-    Student(Student other) {
-        this.name = other.name;
-        this.rno = other.rno;
-        this.marks = other.marks;
-    }
-
-    Student() {
-        this(13, "default person", 100.0f);
-    }
-
-    Student(int rno, String name, float marks) {
-        this.rno = rno;
-        this.name = name;
-        this.marks = marks;
-    }
-
 }
